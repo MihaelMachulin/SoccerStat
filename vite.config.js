ea4e7f67091f51d1ba42dd7417672803c1116be0
MihaelMachulin <mihael.machulin@gmail.com>
2022-02-23 00:55:24 +0300

    With secret - build 0.0.9

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: '/SoccerStat/',
-  define: { 'process.env': 'process.env' }
+  base: '/SoccerStat/'
 })
