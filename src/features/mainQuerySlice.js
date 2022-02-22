477f8641b4711cd8caf8b746f0da1906757a7d2b
MihaelMachulin <mihael.machulin@gmail.com>
2022-02-22 22:51:24 +0300

    With secret - build 0.0.8

--- src/features/mainQuerySlice.js
+++ src/features/mainQuerySlice.js
@@ -5,7 +5,8 @@ export const SoccerApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.football-data.org/v2/",
     prepareHeaders: headers => {
-      headers.set('X-Auth-Token', 'c512bd1adb1947e987dbeff47924c56e' )
+      // headers.set('X-Auth-Token', 'c512bd1adb1947e987dbeff47924c56e' )
+      headers.set('X-Auth-Token', 'VITE_APP_KEY' )
       return headers
     }
   }),
