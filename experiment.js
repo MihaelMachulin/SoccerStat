5a861a0d37dc6c62ef0c052cf46b161ec609bcb0
MihaelMachulin <mihael.machulin@gmail.com>
2022-02-22 18:33:37 +0300

    With secret - build 0.0.1

--- experiment.js
+++ experiment.js
@@ -1,14 +1,27 @@
-var containsDuplicate = function(nums) {
-  let seen = new Set()
+// Есть два сортированных массива с числами.
+// Нужно написать функцию, которая возвращает новый массив,
+// содержащий элементы, которые встречаются в обоих массивах.
 
-  for (let num of nums) {
-    if (seen.includes(num)) return true
-    seen.push(num)
+const a = [ 1, 2, 4, 7, 11, 19 ];
+const b = [ 2, 7, 19, 28, 31 ];
+
+function findEqualElements(arr1, arr2) {
+  const maxLen = arr1.length < arr2.length ? arr2.length : arr1.length
+  let result = [], i = 0, j = 0
+
+  for(let n = 0; n < maxLen; n++) {
+    if (arr1[i] === arr2[j]) {result.push(arr1[i]); }
+    arr1[i] > arr2[j] ? j++ : i++
   }
-return false
-};
+  console.log(result)
+  return result
+}
 
-console.log(containsDuplicate([3,3]))
-//console.log(containsDuplicate([1,2,3,1]))
-//console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
-//console.log(containsDuplicate([1,2,3,4,5,6,7,8]))
+// Примеры
+findEqualElements([1, 2, 3], [2]); // => [2]
+findEqualElements([2], [1, 2, 3]); // => [2]
+findEqualElements([1, 2, 2, 3], [2, 2, 2, 2]); // => [2, 2]
+
+findEqualElements([2], [2, 2]); // => [2]
+findEqualElements([2, 2], [2, 2]); // => [2, 2]
+console.log(findEqualElements(a, b))
