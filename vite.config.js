a9c0ad19e2fcec1575a06ca73ab3d972803d2758
MihaelMachulin <mihael.machulin@gmail.com>
2022-02-23 22:49:20 +0300

    With secret - build 0.0.11

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig(({ command, mode }) => {
   console.log(command)
   if (command === 'serve') {
     return {
-      plugins: [reactRefresh()],
-      origin: 'http://127.0.0.1:8080/'
+      plugins: [reactRefresh()]
     }
   } else  {
     return {
